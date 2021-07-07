34a5af58f0c5bca5789b3f61eb1f91ff65c7339c
Kialan Pillay <kialanpillay@gmail.com>
2021-07-07 19:21:41 +0200

    Refactor project structure

---
+++ app.py
@@ -0,0 +1,6 @@
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Intelligent system for automated share evaluation',
+                                     epilog='Version 0.1')
+    args = parser.parse_args()
