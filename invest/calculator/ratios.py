6056c7357ba8a1953c068f68476f4e1c60b760ac
InsaafDhansay <insaafdhansay@gmail.com>
2021-07-13 12:00:21 +0200

    add ratios - missing 1,4,5,6

---
+++ invest/calculator/ratios.py
@@ -0,0 +1,47 @@
+# Historic Earnings Growth Rate - calc rule 1
+
+# Historic Earnings Compound Annual Growth Rate
+def historic_earnings_cagr(eps_n, eps_prev_x, x):
+    cagr = ((eps_n / eps_prev_x) ** 1 / x) - 1
+    return cagr
+
+
+# Historic Price to Earnings
+def historic_price_to_earnings(price, eps):
+    return price / eps
+
+
+# Forward Earnings - calc rule 4
+
+# Forward Earnings Compound Annual Growth Rate - calc rule 5
+
+# Forward Price to Earnings - calc rule 6
+
+
+# Price to Earnings Relative Sector - calc rule 7, calc 8
+def pe_relative_sector(historic_pe_share, historic_pe_sector):
+    return historic_pe_share / historic_pe_sector
+
+
+# Price to Earnings Relative Market - calc rule 7, calc 9
+def pe_relative_market(historic_pe_share, historic_pe_market):
+    return historic_pe_share / historic_pe_market
+
+
+# Return on Equity -calc rule 8, calc 10
+def return_on_equity(net_income, total_shareholder_equity):
+    return net_income / total_shareholder_equity
+
+
+# Cost of Equity
+def cost_of_equity(market_return_rate, risk_free_return_rate, share_beta):
+    equity_risk_premium = market_return_rate - risk_free_return_rate
+    cost_of_equity = risk_free_return_rate + (share_beta * equity_risk_premium)
+    return cost_of_equity
+
+
+# Relative Debt to Equity
+def relative_debt_to_equity(total_liability, total_shareholder_equity, d_e_industry, ):
+    d_e = total_liability / total_shareholder_equity
+    relative_d_e = d_e / d_e_industry
+    return relative_d_e
