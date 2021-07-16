191a4323a8578055a08acb30f5fce15a874a4c19
Kialan Pillay <kialanpillay@gmail.com>
2021-07-16 16:50:55 +0200

    Update formatting

--- gnn/training/engine.py
+++ gnn/training/engine.py
@@ -2,11 +2,11 @@ import torch
 
 
 class Engine:
-    def __init__(self, model, loss, optim, clip, step_size, horizon, scaler, device, cl=True):
+    def __init__(self, model, criterion, optim, clip, step_size, horizon, scaler, device, cl=True):
         self.scaler = scaler
         self.model = model.to(device)
         self.optim = optim
-        self.loss = loss
+        self.criterion = criterion
         self.clip = clip
         self.step = step_size
         self.iter = 1
@@ -16,14 +16,14 @@ class Engine:
 
     def train(self, model_input, target, idx=None):
         self.optim.zero_grad()
-        forecast = self.model(model_input, idx=idx).transpose(1,3)
+        forecast = self.model(model_input, idx=idx).transpose(1, 3)
         target = torch.unsqueeze(target, dim=1)
         if self.iter % self.step == 0 and self.task_level <= self.seq_out_len:
             self.task_level += 1
         if self.cl:
-            loss = self.loss(forecast[:, :, :, :self.task_level], target[:, :, :, :self.task_level])
+            loss = self.criterion(forecast[:, :, :, :self.task_level], target[:, :, :, :self.task_level])
         else:
-            loss = self.loss(forecast, target)
+            loss = self.criterion(forecast, target)
         loss.backward()
 
         if self.clip is not None:
