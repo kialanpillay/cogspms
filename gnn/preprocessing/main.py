347c89338682a7a36d82d7207bfe074d26ed1557
Kialan Pillay <kialanpillay@gmail.com>
2021-07-21 12:51:46 +0200

    Update default arguments

--- gnn/preprocessing/main.py
+++ gnn/preprocessing/main.py
@@ -18,10 +18,10 @@ def clean(arguments):
                 clean_df[name] = pd.to_numeric(df['Closing (c)'])
                 clean_df[name].fillna(0, inplace=True)
 
-            clean_df.reindex(index=clean_df.index[::-1]).to_csv(arguments.output, index=False)
+            clean_df.reindex(index=clean_df.index[::-1]).to_csv(arguments.output + "_clean.csv", index=False)
             print("Processing Time: {:5.2f}s".format(time.time() - start_time))
 
-        df = pd.read_csv(arguments.output)
+        df = pd.read_csv(arguments.output + "_clean.csv")
         sample_df = df[['PROSUS', 'ANGGOLD', 'NEDBANK', 'SASOL', "VODACOM", "BHP"]]
         corr = sample_df.corr()
         if arguments.plot:
@@ -40,13 +40,12 @@ def clean(arguments):
                 if df_.shape[0] == 1259:
                     clean_df = pd.concat([clean_df, df_], axis=1)
 
-            clean_df.to_csv(arguments.output, index=False)
+            clean_df.to_csv(arguments.output + "_clean.csv", index=False)
             print("Processing Time: {:5.2f}s".format(time.time() - start_time))
 
-        df = pd.read_csv(arguments.output)
+        df = pd.read_csv(arguments.output + "_clean.csv")
         sample_df = df[['AAPL', 'AMZN', 'FB', 'GOOGL', "MSFT", "XRAY"]]
         corr = sample_df.corr()
-        print(corr.to_numpy())
         if arguments.plot:
             sn.heatmap(corr, annot=True)
             plt.show()
@@ -56,7 +55,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--raw', type=str, default='data/all_stocks_5yr.csv')
     parser.add_argument('--raw_folder', type=str, default='data/JSE')
-    parser.add_argument('--output', type=str, default='data/JSE_clean.csv')
+    parser.add_argument('--output', type=str, default='data/JSE')
     parser.add_argument('--plot', type=bool, default=False)
+    parser.add_argument('--noise', type=bool, default=False)
     args = parser.parse_args()
     clean(args)
