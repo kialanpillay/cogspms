c91a1b0b25a0093082fde30f862e80879c6eabd8
Kialan Pillay <kialanpillay@gmail.com>
2021-07-26 12:39:42 +0200

    Update preprocessing data sources

--- gnn/preprocessing/main.py
+++ gnn/preprocessing/main.py
@@ -8,13 +8,16 @@ import seaborn as sn
 
 
 def clean(arguments):
-    if args.raw_folder:
+    if args.raw_folder and args.source == 'SB':
         if not os.path.isfile(arguments.output):
             start_time = time.time()
             clean_df = pd.DataFrame()
-            for path in os.listdir(arguments.raw_folder):
+            dir_ = arguments.raw_folder + '_' + arguments.source
+            for path in os.listdir(dir_):
+                if path == ".DS_Store":
+                    continue
                 name = path[path.index("-") + 1:path.index("2") - 1]
-                df = pd.read_csv(os.path.join(arguments.raw_folder, path), delimiter=';')
+                df = pd.read_csv(os.path.join(dir_, path), delimiter=';')
                 clean_df[name] = pd.to_numeric(df['Closing (c)'])
                 clean_df[name].fillna(0, inplace=True)
 
@@ -28,10 +31,29 @@ def clean(arguments):
             sn.heatmap(corr, annot=True)
             plt.show()
 
+    if args.raw_folder and args.source == 'IRESS':
+        if not os.path.isfile(arguments.output):
+            start_time = time.time()
+            clean_df = pd.DataFrame()
+            dir_ = arguments.raw_folder + '_' + arguments.source
+            for path in os.listdir(arguments.raw_folder + '_' + arguments.source):
+                if path == ".DS_Store":
+                    continue
+                name = path[0:path.index(".")]
+                print(name)
+                df = pd.read_csv(os.path.join(dir_, path), delimiter=';')
+                clean_df[name] = pd.to_numeric(df['Close'])
+
+            clean_df.reindex(index=clean_df.index[::-1]).to_csv(arguments.output + "_clean.csv", index=False)
+            print("Processing Time: {:5.2f}s".format(time.time() - start_time))
+
+        df = pd.read_csv(arguments.output + "_daily_clean.csv")
+        print(df.head())
+
     if "SP500" in args.raw:
         if not os.path.isfile(arguments.output):
             start_time = time.time()
-            df = pd.read_csv(arguments.raw)
+            df = pd.read_csv(arguments.raw + '.csv')
             clean_df = pd.DataFrame()
             names = df['Name'].unique()
 
@@ -53,8 +75,9 @@ def clean(arguments):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--raw', type=str, default='data/SP500_raw.csv')
+    parser.add_argument('--raw', type=str, default='')
     parser.add_argument('--raw_folder', type=str, default='data/JSE')
+    parser.add_argument('--source', type=str, default='SB')
     parser.add_argument('--output', type=str, default='data/JSE')
     parser.add_argument('--plot', type=bool, default=False)
     parser.add_argument('--noise', type=bool, default=False)
