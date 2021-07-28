216671fe3f7f472f360bbcebe36e86e3976c47b2
Kialan Pillay <kialanpillay@gmail.com>
2021-07-28 10:57:35 +0200

    Update data loading

--- invest/preprocessing/dataloader.py
+++ invest/preprocessing/dataloader.py
@@ -8,14 +8,8 @@ companies = ["ADVTECH", "CITY LODGE HOTELS", "CLICKS GROUP", "CURRO HOLDINGS", "
              "NAMPAK", "PPC", "RAUBEX GROUP", "REUNERT", "SUPER GROUP", "TRENCOR", "WLSN.BAYLY HOLMES-OVCON"]
 
 
-def load_data():
-    df = pd.read_csv("data/invest_data.csv", sep=';')
-    mask = (df['Date'] > '2012-01-01')
-    int_df = df.loc[df['Name'].isin(companies)]  # 36 Shares
-    final_df = int_df.loc[mask]
-    print(final_df['Name'].unique())
-    print(final_df['Name'].nunique())
-    print(final_df)
+def load_data(filename='data/INVEST_clean.csv'):
+    return pd.read_csv(filename, sep=',')
 
 
 def load_dummy_data():
@@ -25,9 +19,7 @@ def load_dummy_data():
     df['PESector'] = [x.replace(',', '.') for x in df['PESector']]
     df['MarketRateOfReturn'] = [x.replace(',', '.') for x in df['MarketRateOfReturn']]
     df['RiskFreeRateOfReturn'] = [x.replace(',', '.') for x in df['RiskFreeRateOfReturn']]
-    df['Share Beta'] = [x.replace(',', '.') for x in df['Share Beta']]
-    df['Shareholders Equity'] = [x.replace(',', '.') for x in df['Shareholders Equity']]
-    df['Inflation Rate'] = [x.replace(',', '.') for x in df['Inflation Rate']]
-    df_trimmed = df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
-
-    return df_trimmed
+    df['ShareBeta'] = [x.replace(',', '.') for x in df['ShareBeta']]
+    df['ShareholdersEquity'] = [x.replace(',', '.') for x in df['ShareholdersEquity']]
+    df['InflationRate'] = [x.replace(',', '.') for x in df['InflationRate']]
+    return df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
