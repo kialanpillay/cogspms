2e676ed58df98e0ce849ef53badc54fde1ec68a6
Kialan Pillay <kialanpillay@gmail.com>
2021-08-02 11:56:21 +0200

    Update adjacency matrix DataFrame index

--- gnn/evaluation/test_.py
+++ gnn/evaluation/test_.py
@@ -47,13 +47,15 @@ def custom_test(test_data, args, result_train_file, result_test_file):
     with open(os.path.join(result_train_file, 'norm_stat.json'), 'r') as f:
         normalize_statistic = json.load(f)
     model = load_model(result_train_file)
+
     adj = model.final_adj[0].detach().cpu().numpy()
     sn.set(font_scale=0.5)
     columns = pd.read_csv('data/JSE_clean_truncated.csv').columns
     df = pd.DataFrame(data=adj, columns=columns)
-    df.index = columns
+    df.index = columns.values
     sn.heatmap(pd.DataFrame(data=adj, columns=columns), annot=False, center=0, cmap='coolwarm', square=True)
     plt.savefig(os.path.join('img', args.model + '_corr.png'), dpi=300, bbox_inches='tight')
+
     x, y = process_data(test_data, args.window_size, args.horizon)
     scaler = gnn.preprocessing.loader.CustomStandardScaler(mean=x.mean(), std=y.std())
     test_loader = gnn.preprocessing.loader.CustomSimpleDataLoader(scaler.transform(x), scaler.transform(y),
