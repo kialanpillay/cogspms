5c68edb997c642d574c963ac56a78df429ca55e4
Kialan Pillay <kialanpillay@gmail.com>
2021-08-04 20:57:10 +0200

    Save figures

--- gnn/analysis/main.py
+++ gnn/analysis/main.py
@@ -1,4 +1,5 @@
 import argparse
+import os
 
 import matplotlib.pyplot as plt
 import networkx as nx
@@ -28,7 +29,7 @@ def run():
     if args.plot:
         sn.set(font_scale=0.5)
         sn.heatmap(df_cluster, annot=False, center=0, cmap='coolwarm', square=True)
-
+        plt.savefig(os.path.join('img', 'bicluster_n_2.png'), dpi=300, bbox_inches='tight')
         if graph:
             pos = nx.spring_layout(graph, seed=args.seed)
             betweenness_dict = nx.betweenness_centrality(graph, normalized=True, endpoints=True)
@@ -40,8 +41,7 @@ def run():
             # for node, (x, y) in pos.items():
             #     text(x, y, node, fontsize=dict(graph.degree)[node], ha='center', va='center')
             plt.axis('off')
-
-        plt.show()
+            plt.savefig(os.path.join('img', 'network.png'), dpi=300, bbox_inches='tight')
 
 
 def str2bool(v):
