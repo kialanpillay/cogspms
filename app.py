44327e89c9307723350afb407e6522ee35c464c8
InsaafDhansay <insaafdhansay@gmail.com>
2021-08-07 19:19:13 +0200

    add noise simulation function

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import argparse
 import json
 import time
-
+import numpy as np
 import pandas as pd
 
 import invest.evaluation.validation as validation
@@ -60,9 +60,14 @@ def main():
 
     for year in range(2015, 2018):
         print(year, "IP.JGIND", len(investable_shares_jgind[str(year)]), investable_shares_jgind[str(year)])
+    ratios_jgind = []
+    ip_ar_jgind, ip_cr_jgind, ip_aar_jgind, ip_tr_jgind, ip_sr_jgind = validation.process_metrics(df_,
+                                                                                                  prices_current_jgind,
+                                                                                                  prices_initial_jgind,
+                                                                                                  share_betas_jgind,
+                                                                                                  2015, 2018, "JGIND")
+    ratios_jgind.extend([ip_cr_jgind*100, ip_aar_jgind*100, ip_tr_jgind, ip_sr_jgind])
 
-    validation.process_metrics(df_, prices_current_jgind, prices_initial_jgind, share_betas_jgind,
-                               2015, 2018, "JGIND")
     validation.process_benchmark_metrics(2015, 2018, "JGIND")
 
     for year in range(2015, 2018):
@@ -93,13 +98,19 @@ def main():
 
     for year in range(2015, 2018):
         print(year, "IP.JCSEV", len(investable_shares_jcsev[str(year)]), investable_shares_jcsev[str(year)])
+    ratios_jcsev = []
+    ip_ar_jcsev, ip_cr_jcsev, ip_aar_jcsev, ip_tr_jcsev, ip_sr_jcsev = validation.process_metrics(df_,
+                                                                                                  prices_current_jcsev,
+                                                                                                  prices_initial_jcsev,
+                                                                                                  share_betas_jcsev,
+                                                                                                  2015, 2018, "JCSEV")
+    ratios_jcsev.extend([ip_cr_jcsev*100, ip_aar_jcsev*100, ip_tr_jcsev, ip_sr_jcsev])
 
-    validation.process_metrics(df_, prices_current_jcsev, prices_initial_jcsev, share_betas_jcsev,
-                               2015, 2018, "JCSEV")
     validation.process_benchmark_metrics(2015, 2018, "JCSEV")
     hours, rem = divmod(end - start, 3600)
     minutes, seconds = divmod(rem, 60)
     print("Experiment time taken: ""{:0>2}:{:0>2}:{:05.2f}".format(int(hours), int(minutes), seconds))
+    return ratios_jgind, ratios_jcsev  # return ratio list excluding aar for simulation
 
 
 def str2bool(v):
@@ -124,4 +135,24 @@ if __name__ == '__main__':
     parser.add_argument("--network", type=str, default='v')
     parser.add_argument("--gnn", type=str2bool, default=False)
     args = parser.parse_args()
-    main()
+    if args.noise:
+        jgind_ratios = []
+        jcsev_ratios = []
+        for i in range(0, 10):
+            ratios_jgind, ratios_jcsev = main()
+            jgind_ratios.append(ratios_jgind)
+            jcsev_ratios.append(ratios_jcsev)
+        jgind_averaged_ratios = np.mean(jgind_ratios, axis=0)
+        jcsev_averaged_ratios=np.mean(jcsev_ratios, axis=0)
+        print(jgind_averaged_ratios)
+        print(jcsev_averaged_ratios)
+
+
+
+
+
+
+
+
+    else:
+        main()
