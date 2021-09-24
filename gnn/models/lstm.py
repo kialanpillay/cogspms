d2eb7462c78b7619f52a7dbafecbaddd57dca968
Kialan Pillay <kialanpillay@gmail.com>
2021-09-24 16:26:55 +0200

    Update LSTM hyperparameters

--- gnn/models/lstm.py
+++ gnn/models/lstm.py
@@ -4,10 +4,10 @@ import torch.nn as nn
 
 class LSTM(nn.Module):
     """
-    A baseline 100-layer LSTM model with a single output layer for univariate predictions
+    A baseline LSTM model with a single output layer for single-node predictions
     """
 
-    def __init__(self, input_size=1, hidden_layers=100, output_size=1):
+    def __init__(self, input_size=1, hidden_layers=1, hidden_size=100, output_size=1):
         """
         Parameters
         ----------
@@ -20,13 +20,14 @@ class LSTM(nn.Module):
         """
         super().__init__()
         self.hidden_layers = hidden_layers
+        self.hidden_size = hidden_size
 
-        self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_layers)
+        self.lstm = nn.LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=hidden_layers)
 
-        self.linear = nn.Linear(hidden_layers, output_size)
+        self.linear = nn.Linear(hidden_size, output_size)
 
-        self.hidden_cell = (torch.zeros(1, 1, self.hidden_layers),
-                            torch.zeros(1, 1, self.hidden_layers))
+        self.hidden_cell = (torch.zeros(self.hidden_layers, 1, self.hidden_size),
+                            torch.zeros(self.hidden_layers, 1, self.hidden_size))
 
     def forward(self, in_seq):
         lstm_out, self.hidden_cell = self.lstm(in_seq.view(len(in_seq), 1, -1), self.hidden_cell)
